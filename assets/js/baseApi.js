36d975a4405ceee80f4a00291f983d3cda77df9b
zhongyu <zy2022743073@163.com>
2022-09-29 15:47:39 +0800

    登录注册页面基本完成

---
+++ assets/js/baseApi.js
@@ -0,0 +1,20 @@
+$.ajaxPrefilter(function(config){
+
+  const format2Json = (source)=>{
+    let target = {}
+    source.split('&').forEach((el)=>{
+      let kv = el.split('=')
+      target[kv[0]] = kv[1]
+    })
+    return JSON.stringify(target)
+  }
+
+  //统一设置基准地址
+  config.url = 'http://big-event-vue-api-t.itheima.net' + config.url
+
+  //统一设置请求头Content-Type值
+  config.contentType = 'application/json'
+
+  //统一设置请求的参数-post请求
+  config.data = format2Json(config.data)
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,76 @@
-$(function(){
+$(function () {
   //点击注册
-  $('#link_reg').on('click',function(){
+  $('#link_reg').on('click', function () {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
   //点击登录
-  $('#link_login').on('click',function(){
+  $('#link_login').on('click', function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
 
-  
+
+
+  let form = layui.form
+  let layer = layui.layer
+  //自定义校验规则
+  form.verify({
+    pwd: [/^[\S]{6,12}$/, '密码为6~12位,且不能出现空格'],
+    //校验两次密码是否一致
+    repwd: function (value) {
+      var pwd = $('.reg-box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+
+      }
+
+    }
+  })
+
+  //监听注册表单提交事件
+  $('#form_reg').on('submit',function(e){
+    e.preventDefault()
+    $.ajax({
+      method:'POST',
+      url:'/api/reg',
+      // contentType:'application/json',
+      /* data:JSON.stringify({
+        username:$('#form_reg [name=username]').val(),
+        password:$('#form_reg [name=password]').val(),
+        repassword:$('#form_reg [name=repassword]').val()
+      }), */
+      data:$(this).serialize(),
+      success(res){
+        if(res.code !== 0) return layer.msg(res.message)
+        layer.msg('注册成功')
+        $('#link_login').trigger('click')
+      }
+    })
+
+  })
+
+  //监听登录事件
+  $('#form_login').on('submit',function(e){
+    e.preventDefault()
+    $.ajax({
+      method:'POST',
+      url:'/api/login',
+      // contentType:'application/json',
+      data:$(this).serialize(),
+      success(res){
+        if(res.code !==0) return layer.msg(res.message)
+
+        localStorage.setItem('big_news_token',res.token)
+
+        location.href = '/index.html'
+      }
+    })
+
+  })
+
+
+
 })
+
